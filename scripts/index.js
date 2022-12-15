6b60865b174ec64e616a222feb00d10823317d2a
Ilia Ivashkin <lilwoozzy@gmail.com>
2022-12-15 16:13:20 +0800

    after review

--- scripts/index.js
+++ scripts/index.js
@@ -3,8 +3,8 @@ const closePop = document.querySelector(".popup__close");
 let popForm = document.querySelector(".popup__form");
 let popUp = document.querySelector(".popup");
 const submitPop = document.querySelector(".popup__save");
-let nameInput = popForm.querySelector(".popup__input-name");
-let jobInput = popForm.querySelector(".popup__input-bio");
+let nameInput = popForm.querySelector(".popup__input_type_name");
+let jobInput = popForm.querySelector(".popup__input_type_bio");
 let profileName = document.querySelector(".profile__name");
 let profileBio = document.querySelector(".profile__bio");
 
